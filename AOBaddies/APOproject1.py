f7cd7e3ed2788f200e6197d7b3bffcd8e2a1cb6c
lexiathilla <att22@ic.ac.uk>
2025-12-07 12:16:35 +0000

    added the excel access and additional rules on aromaticity

--- AOBaddies/APOproject1.py
+++ AOBaddies/APOproject1.py
@@ -1,58 +1,81 @@
 from pyomo.environ import *
 import math
 import matplotlib.pyplot as plt
+import pandas as pd
 model = ConcreteModel('APOProject')
+inputGCs="C:\Users\Alexia\OneDrive - Imperial College London\AAYEAR4\APO1\GCs.xlsx"
+
+#Excel Reads
+df = pd.read_excel(inputGCs, sheet_name="Group")
+df.columns = df.columns.str.strip()   # Clean spaces
+
+dfcp=pd.read_excel(inputGCs, sheet_name="CpGC")
+dfcp.columns = dfcp.columns.str.strip()   # Clean spaces
+
+dfSVSH = pd.read_excel("GCs.xlsx", sheet_name="SVSH", index_col=0)
+dfSVSH.columns = dfSVSH.columns.str.strip()
+dfSVSH.index = dfSVSH.index.str.strip()
 
 #max number of groups per elements
 ni_max=8
 ng_max=10
 
 #Define SETS
-model.g=Set(initialize=['CH3', 'CH2','CH','C','CHdouble','Cdouble','Cdouble','NH2','NH','N','Ndouble','OH','O','Odouble'])#groups for GC Cp
-model.i=Set(initialize=['CH3','CH2','CH','C','CH2doubleCH','CHdoubleCH','CH2doubleC'])#First order groups for GC - need to choose from large variety - litterature backed ?
+model.g = Set(initialize=dfcp['Group'].unique().tolist())#groups for GC Cp
+model.i = Set(initialize=df['Group'].unique().tolist())#First order groups for GC - need to choose from large variety - litterature backed ?
 model.A=Set(initialize=['Cp'])
-model.X=Set(initialize=['Tm','Tb','molarvol','sol1','sol2','sol3']) #The parameters calculated from MG method - might actually just be f(X) or not could put different constraints definition for some X 
-model.B=Set(initialize=['acyclic','monocyclic','bicyclic'])
+X_props = ['Tb1i','Tm1i','δD1i','δP1i','δH1i','Vm1i']
+model.X = Set(initialize=X_props)#The parameters calculated from MG method - might actually just be f(X) or not could put different constraints definition for some X 
+model.B=Set(initialize=['acyclic','monocyclic'])#No'bicyclic'
 
 #Ki={i: math.ceil(math.log2(ni)) for i in model.i}  could create ngs and nis for each g and i btw
 #Kg={g: math.ceil(math.log2(ng)) for g in model.g}
+
+# Bit-lengths for binary expansion (correct Set initialization)
 Ki = math.ceil(math.log2(ni_max))
 Kg = math.ceil(math.log2(ng_max))
 # Create index sets
-model.Ki = Set(1, Ki)
-model.Kg = Set(1, Kg)
+model.Ki = Set(1, Ki + 1)
+model.Kg = Set(1, Kg + 1)
 
 #Define Parameters GC, ideally for each grp considered and each property (i,X)
-i_data={('CH3', 'Tm'): 1, ('CH2', 'Tm'): 2, ('CH', 'Tm'): 2, ('C', 'Tm'): 1, ('CH2doubleCH', 'Tm'): 1,('CHdoubleCH', 'Tm'): 1,('CH2doubleC', 'Tm'): 1
-        ,('CH3', 'Tb'): 1, ('CH2', 'Tb'): 2, ('CH', 'Tb'): 2, ('C', 'Tb'): 1, ('CH2doubleCH', 'Tb'): 1,('CHdoubleCH', 'Tb'): 1,('CH2doubleC', 'Tb'): 1
-        ,('CH3', 'molarvol'): 1, ('CH2', 'molarvol'): 2, ('CH', 'molarvol'): 2, ('C', 'molarvol'): 1,('CH2doubleCH', 'molarvol'): 1,('CHdoubleCH', 'molarvol'): 1,('CH2doubleC', 'molarvol'): 1
-        ,('CH3', 'sol1'): 1, ('CH2', 'sol1'): 2, ('CH', 'sol1'): 2, ('C', 'sol1'): 1, ('CH2doubleCH', 'sol1'): 1,('CHdoubleCH', 'sol1'): 1,('CH2doubleC', 'sol1'): 1
-        ,('CH3', 'sol2'): 1, ('CH2', 'sol2'): 2, ('CH', 'sol2'): 2, ('C', 'sol2'): 1, ('CH2doubleCH', 'sol2'): 1,('CHdoubleCH', 'sol2'): 1,('CH2doubleC', 'sol2'): 1
-        ,('CH3', 'sol3'): 1, ('CH2', 'sol3'): 2, ('CH', 'sol3'): 2, ('C', 'sol3'): 1,('CH2doubleCH', 'sol3'): 1,('CHdoubleCH', 'sol3'): 1,('CH2doubleC', 'sol3'): 1}
-model.ci=Param(model.i, model.X, initialize=i_data, default=0)
+i_data = {}
+for _, row in df.iterrows():
+    i = row['Group']
+    for X in X_props:
+        i_data[(i, X)] = float(row[X])
+model.ci = Param(model.i, model.X, initialize=i_data, default=0)
 
-#for Cp
-g_data={('CH3', 'Cp'): 1, ('CH2', 'Cp'): 2, ('CH', 'Cp'): 2, ('C', 'Cp'): 1, ('CHdouble', 'Cp'): 1,('Cdouble', 'Cp'): 1,('NH2', 'Cp'): 1,('NH', 'Cp'): 1,('N', 'Cp'): 1,('Ndouble', 'Cp'): 1,('OH', 'Cp'): 1,('O', 'Cp'): 1,('Odouble', 'Cp'): 1 }
-model.cg=Param(model.g, model.A, initialize=g_data, default=0)
+#for Cp, group contributions
+g_data = { (g, 'Cp'): float(dfcp.loc[g, 'Cp'])
+           for g in model.g if 'Cp' in dfcp.columns }
+model.cg = Param(model.g, model.A, initialize=g_data, default=0)
+
 
 #valency data (defined for i for now)
-v_i={'CH3':1, 'CH2':2, 'CH':3, 'C':4, 'CH2doubleCH':1, 'CHdoubleCH':3, 'CH2doubleC':2}
-model.vi=Param(model.i, initialize=v_i, default=0)
-v_g={}
-model.vg=Param(model.i, initialize=v_g, default=0)
-#group types : 1=general, 2=unsaturated, 3=aromatic; probably wrong way to do it 
-type_data={'CH3':1, 'CH2':1, 'CH':1, 'C':1, 'CH2doubleCH':2, 'CHdoubleCH':2, 'CH2doubleC':2}
-model.grouptype = Param(model.i, initialize=type_data, default=0)
+valency_dict = df.set_index('Group')['Valency 1'].to_dict()
+model.vi = Param(model.i, initialize=valency_dict, default=0)
+#v_g={}
+#model.vg=Param(model.i, initialize=v_g, default=0)
 
+#group types mapping ; probably wrong way to do it : Just directly use column
+TYPE_COL = "Type of molecule (like aromatic and so on) ?"
+
+# Build a dict: Group -> type string (read exactly as in Excel)
+type_str_dict = df.set_index('Group')[TYPE_COL].to_dict()
+
+# Create Pyomo sets from the dict (no normalization)
+model.Ga = Set(initialize=[i for i, t in type_str_dict.items() if t == 'aromatic'])  # aromatic groups
+model.Gc = Set(initialize=[i for i, t in type_str_dict.items() if t == 'cyclic'])    # cyclic groups
+model.Ggen = Set(initialize=[i for i, t in type_str_dict.items() if t == 'general']) # general groups
+model.Ggunsat = Set(initialize=[i for i, t in type_str_dict.items() if t == 'unsat']) # general groups
 #Number of groups in other groups (link between Cp and Hurekkikar et al) #TO CORRECT, when relation not mentionned we get zero
-groups_data={('CH3', 'CH3'): 1,
-             ('CH2', 'CH2'): 1, 
-             ('CH', 'CH'): 1, 
-             ('C', 'C'): 1, 
-             ('CH2doubleCH', 'CHdouble'): 1,('CH2doubleCH', 'CH2'): 1,
-             ('CHdoubleCH', 'CHdouble'): 2,
-             ('CH2doubleC', 'CH2'): 1,('CH2doubleC', 'Cdouble'): 1 }
-model.ig=Param(model.i, model.g, initialize=groups_data, default=0)
+ig_dict = {
+    (i, g): int(dfSVSH.loc[i, g])
+    for i in dfSVSH.index
+    for g in dfSVSH.columns
+}
+model.ig = Param(model.i, model.g, initialize=ig_dict, default=0)
 
 #----DEFINE VARIABLES--- ##WORK ON PROPERLY DEFINING BOUNDS
 model.Tm=Var(within=NonNegativeReals, bounds=(0.01, 313)) #in K
@@ -67,9 +90,15 @@ model.molarvol=Var(within=NonNegativeReals, bounds=(0.0001, None)) #in K
 #Set of binary variables (we'll later define integar cuts to get rid of some solutions, also the variables are defined through linear expressions below)
 model.yi = Var(model.i, model.Ki, within=Binary)
 model.yg = Var(model.g, model.Kg, within=Binary)
-#Binary molecule types
+
+#Binary molecule types : molecule type selection (acyclic vs monocyclic)
 model.yb=Var(model.B, within=Binary) #binary molecule types defined later
 model.m=Var(within=Reals)
+
+#aromatic or cyclic mode 
+model.ya = Var(within=Binary)  # aromatic mode
+model.yc = Var(within=Binary)  # cyclic (non-aromatic) mode
+
 #Integer counts (calculated with binary variables)
 model.ni=Var(model.i, within=NonNegativeReals)#!!! watch w the integer cuts thing might have to def ni=sumk=0 to k 2^kyik (continuous associated with a set of binary variables)
 model.ng = Var(model.g, within=NonNegativeReals)
@@ -99,7 +128,7 @@ model.w_Cp  = Param(initialize=1.0)     # weight for heat capacity
 # --- Constraints ---
 # (1) property group contribution relationship
 def XGC_rule(model, X):
-    return model.fX[X]==(sum(model.ni[i] * model.ci[i]) for i in model.i)
+    return model.fX[X]==(sum(model.ni[i] * model.ci[i, X]) for i in model.i)
 model.XGC=Constraint(model.X,rule=XGC_rule)
 
 # (2) Number of g-groups derived from i-groups
@@ -112,18 +141,22 @@ def CpGC_rule(model):#either we define specific exceptions to take into account 
     return model.Cp == sum(model.ng[g] * model.cg[g, 'Cp'] for g in model.g)
 model.CpGC = Constraint(rule=CpGC_rule)
 
-# (4) def the various functions in Hurekkikar group contributions
+# (4) def the various functions in Hurekkikar group contributions, not sure it properly relates to our properties definitions
 def link_properties_rule(model, X):
-    if X == 'Tm':
-        return model.Tm == exp(model.fX[X]/model.Tm0)
-    elif X == 'Tb':
-        return model.Tb == exp(model.fX[X]/model.Tb0)
-    elif X == 'molarvol':
-        return model.molarvol == model.fX[X]+model.Vm0
-    elif X in ['sol1', 'sol2', 'sol3']:
-        return getattr(model,X) == model.fX[X]
+    if X == 'Tm1i':
+        return model.Tm == exp(model.fX[X] / model.Tm0)
+    elif X == 'Tb1i':
+        return model.Tb == exp(model.fX[X] / model.Tb0)
+    elif X == 'Vm1i':
+        return model.molarvol == model.fX[X] + model.Vm0
+    elif X == 'δD1i':
+        return model.sol1 == model.fX[X]
+    elif X == 'δP1i':
+        return model.sol2 == model.fX[X]
+    elif X == 'δH1i':
+        return model.sol3 == model.fX[X]
     else:
-        return Constraint.Skip    
+        return Constraint.Skip
 model.link_properties = Constraint(model.X, rule=link_properties_rule)
 
 # (5) Definition of density from GCS calculated parameters
@@ -147,7 +180,7 @@ model.one_type = Constraint(rule=one_type)
 
 #(8) Relating m to binary molecule types:
 def m_rule(model):
-    return model.m == model.yb['acyclic'] - model.yb['bicyclic']
+    return model.m == model.yb['monocyclic']
 model.m_rule = Constraint(rule=m_rule)
 
 #(9) Replacing integer variables with linear expressions : i 
@@ -155,7 +188,7 @@ def ni_rule(model,i):
     return model.ni[i] == sum(2**(k-1)*model.yi[i,k] for k in model.Ki)
 model.ni_rule = Constraint(rule=ni_rule)
 
-#(10) Replacing integer variables with linear expressions : g
+#(10) Replacing integer variables with linear expressions : g ###Maybe obsolete ?
 def ng_rule(model,g):
     return model.ng[g] == sum(2**(k-1)*model.yg[g,k] for k in model.Kg)
 model.ng_rule = Constraint(rule=ng_rule)
@@ -165,10 +198,25 @@ def vi_rule(model):
     return 2*model.m==sum((2-model.vi[i])*model.ni[i] for i in model.i)
 model.vi_rule = Constraint(rule=vi_rule)
 
-#(12) Octet Rule, g compounds
-def vg_rule(model):
-    return 2*model.m==sum((2-model.vg[g])*model.ng[g] for g in model.g)
-model.vg_rule = Constraint(rule=vg_rule)
+#(12) one cyclic mode
+# Exactly one mode if monocyclic, none if acyclic
+def monocyclic_mode_selection_rule(model):
+    return model.ya + model.yc == model.yb['monocyclic']
+model.monocyclic_mode_selection = Constraint(rule=monocyclic_mode_selection_rule)
+
+#(13)aromaticity
+# Exactly 6 aromatic groups if aromatic mode; 0 aromatic groups otherwise
+def aromatic_ring_rule(model):
+    return sum(model.ni[i] for i in model.Ga) == 6 * model.ya
+model.aromatic_ring = Constraint(rule=aromatic_ring_rule)
+
+# Forbid cyclic groups unless cyclic mode is chosen (Big-M)
+M_groups = ni_max * max(1, len(model.i))  # safe upper bound
+model.M_groups = Param(initialize=M_groups)
+
+def cyclic_only_if_cyclic_mode_rule(model):
+    return sum(model.ni[i] for i in model.Gc) <= model.M_groups * model.yc
+model.cyclic_only_if_cyclic_mode = Constraint(rule=cyclic_only_if_cyclic_mode_rule)
 
 #Normalized expressions using tanh(kx)
 model.rho_norm = Expression(expr=tanh( model.rho * model.k_rho))##maybe not we want the larger it is the smaller it is so could put a sorth of inverse there instead of w the weights
@@ -184,7 +232,7 @@ def objective_rule(model):
     )
 model.obj = Objective(rule=objective_rule, sense=minimize)
 #Solve
-solver=SolverFactory('gams:conopt')
+solver=SolverFactory('gams:ANTIGONE')#NOT sure if properly defined
 results=solver.solve(model, tee=True)
 
 #Display results
